e8b83d85d4e096b3cad45862cef5452f734f8286
amy-maria <122695916+amy-maria@users.noreply.github.com>
2025-04-23 15:00:47 -0400

    updated

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
+from flask import request
 
 app = Flask(__name__)
 
@@ -6,3 +7,13 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return render_template("index.html")
+
+
+@app.route("/submit_form", methods=["POST", "GET"])
+def submit_form():
+    if request.method == "POST":
+        data = request.form.to_dict()
+        print(data)
+        return redirect("/thankyou.html")
+    else:
+        print("Error. Message was not submitted.")
